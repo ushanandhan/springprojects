399bc5c2d9fd2319c0e9c9e1aca21b8108f47c26
Ushanandhan <ariyaushan@gmail.com>
2018-09-28 09:18:38 +0530

    Update FizzBuzzStepsDefs.java

--- Spring-Boot-Cucumber/src/test/java/com/example/FizzBuzzStepsDefs.java
+++ Spring-Boot-Cucumber/src/test/java/com/example/FizzBuzzStepsDefs.java
@@ -1,5 +1,27 @@
 package com.example;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class FizzBuzzStepsDefs {
 
+  FizzBuzz fizzBuzz;
+	String fizzBuzzString;
+	
+	@Given("^Create a FizzBuzz game play$")
+	public void create_a_FizzBuzz_game_play() throws Throwable {
+	    fizzBuzz = new FizzBuzz();
+	}
+
+	@When("^I Play with number (\\d+)$")
+	public void i_Play_with_number(int number) throws Throwable {
+	    fizzBuzzString = fizzBuzz.play(number);
+	}
+
+	@Then("^The result is \"([^\"]*)\"$")
+	public void the_result_is(String resultString) throws Throwable {
+	    Assertions.assertEquals(resultString, fizzBuzzString);
+	}
+  
 }
