a04b7e2b6b817cf11d12d4335eff3bb4632be183
Ushanandhan <ariyaushan@gmail.com>
2018-10-01 16:04:15 +0530

    Update CheckRequest.java

--- Spring-Boot-Camel-Camunda/src/main/java/com/example/camunda/component/CheckRequest.java
+++ Spring-Boot-Camel-Camunda/src/main/java/com/example/camunda/component/CheckRequest.java
@@ -1,11 +1,21 @@
 package com.example.camunda.component;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.Execution;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.example.camel.model.Order;
 
 @Component("checkRequest")
 public class CheckRequest {
 
-	public void loanRequest() {
-		System.out.println("Loan Request is processed......");
+	@Autowired
+	RuntimeService runtimeService;
+	
+	public void checkLoanRequest(Execution execution) {
+		System.out.println("Checking loan Request..........."+execution);
+		Order order = (Order) runtimeService.getVariable(execution.getProcessInstanceId(), "order");
+		System.out.println(order.getOrderId());
 	}
 }
