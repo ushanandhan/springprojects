17c4be1f683bf4b70e831b9e4267f62d8c09a2b0
Ushanandhan <ariyaushan@gmail.com>
2018-10-02 08:35:57 +0530

    Working flow of BPMN2 is updated

--- Spring-Boot-Camel-Camunda/src/main/java/com/example/camunda/component/InterviewProcessEngine.java
+++ Spring-Boot-Camel-Camunda/src/main/java/com/example/camunda/component/InterviewProcessEngine.java
@@ -11,8 +11,22 @@ public class InterviewProcessEngine {
 	@Autowired
 	RuntimeService runtimeService;
 	
-	public void SubmitApplication(Execution execution) {
+	public void submitApplication(Execution execution) {
 		String candidateId = (String) runtimeService.getVariable(execution.getProcessInstanceId(), "candidateId");
 		System.out.println("Application for "+candidateId+" is successfully Submitted....");
 	}
+	
+	public boolean isCalledForInterview(Execution execution) {
+		boolean isCalled = false;
+		String candidateId = (String) runtimeService.getVariable(execution.getProcessInstanceId(), "candidateId");
+		if(candidateId.startsWith("CTS")) {
+			isCalled = true;
+		}
+		return isCalled;
+	}
+	
+	public void attendInterview(Execution execution) {
+		System.out.println("Successfully attened interview.....");
+	}
+	
 }
