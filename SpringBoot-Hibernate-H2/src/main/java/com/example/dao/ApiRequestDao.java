6e5e778ebc6fb00f87cd04324d7caca61deef53b
ushanandhan <ariyaushan@gmail.com>
2019-07-30 23:04:58 +0530

    Hibernate related code committed

--- SpringBoot-Hibernate-H2/src/main/java/com/example/dao/ApiRequestDao.java
+++ SpringBoot-Hibernate-H2/src/main/java/com/example/dao/ApiRequestDao.java
@@ -34,3 +34,4 @@ public class ApiRequestDao {
 			entityManager.remove(apiRequest);
 		}
 	}
+}
--- SpringBoot-Hibernate-H2/src/main/java/com/example/service/ApiRequestService.java
+++ SpringBoot-Hibernate-H2/src/main/java/com/example/service/ApiRequestService.java
@@ -1,11 +1,13 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.dao.ApiRequestDao;
 import com.example.entity.ApiRequest;
 
+@Service
 @Transactional
 public class ApiRequestService {
 
