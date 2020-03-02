1351f57e525f9fb3d2f60f7cd5a9a36a22063b15
ushanandhan <ariyaushan@gmail.com>
2020-03-02 22:40:50 +0530

    Spock framework with Junit is added

--- SpringBoot-Gradle-Groovy-Example/src/test/groovy/com/example/demo/controller/EmployeeControllerSpecsTest.groovy
+++ SpringBoot-Gradle-Groovy-Example/src/test/groovy/com/example/demo/controller/EmployeeControllerSpecsTest.groovy
@@ -1,11 +1,42 @@
 package com.example.demo.controller
 
+import com.example.demo.model.Employee
+import com.example.demo.model.Employees
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 import spock.lang.Specification
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class EmployeeControllerSpecsTest extends Specification{
 
-    def "one plus one should equal two"() {
-        expect:
-        1 + 1 == 2
+    @Autowired
+    TestRestTemplate restTemplate
+
+    def "get the list of Employees"() {
+
+        when:
+        def response = restTemplate.getForEntity('/employees/', Employees)
+
+        then:
+        response.statusCode == HttpStatus.OK
+        response.body!=null
+        println(response.body.employeeList)
+    }
+
+    def "Add employee"(){
+        given:
+        Employee employee = new Employee(id: 4,firstName: "kevin",lastName: "ruth",email: "kevin.ruth@email.com")
+        Employees employees = new Employees()
+        employees.employeeList.add(employee)
+
+        when:
+        def response = restTemplate.postForEntity('/employees/',employees,Object)
+
+        then:
+        response.statusCode == HttpStatus.CREATED
     }
 }
