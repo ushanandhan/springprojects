d5303e98b52f6c20b4732c6e3847efb1ebb7f2d9
USHANANDHAN <ariyaushan@gmail.com>
2020-03-02 20:32:20 +0530

    Spock framework with Junit is added

---
+++ SpringBoot-Gradle-Groovy-Example/src/test/groovy/com/example/demo/controller/EmployeeControllerSpecsTest.groovy
@@ -0,0 +1,11 @@
+package com.example.demo.controller
+
+import spock.lang.Specification
+
+class EmployeeControllerSpecsTest extends Specification{
+
+    def "one plus one should equal two"() {
+        expect:
+        1 + 1 == 2
+    }
+}
